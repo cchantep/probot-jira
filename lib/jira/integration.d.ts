9d18f4614c07971a112b1bb62a799de275a100af
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-10-25 16:38:19 +0200

    Minor release 1.0.19

--- lib/jira/integration.d.ts
+++ lib/jira/integration.d.ts
@@ -9,6 +9,7 @@ declare type EventContext = {
     repo: InstallRepo;
     github: GitHubAPI;
     log: LoggerWithTarget;
+    githubDispatchBaseUrl: string;
 };
 export declare function ensureHook(ctx: EventContext): Promise<InstallRepo>;
 export declare function credentials(owner: string, repo: string): Promise<j.Credentials>;
