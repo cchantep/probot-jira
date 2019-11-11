761fcb343b6ce053db9a9e4f68a3da67ce42284e
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-11-11 16:12:26 +0100

    Close #5 - Bump prettier from 1.18.2 to 1.19.1 (#6)

--- src/util.ts
+++ src/util.ts
@@ -2,5 +2,8 @@ import { fold, Either } from 'fp-ts/lib/Either'
 import { Errors } from 'io-ts'
 
 export function fromEither<T>(e: Either<Errors, T>): Promise<T> {
-  return fold(cause => Promise.reject(new Error(JSON.stringify(cause))), (res: T) => Promise.resolve(res))(e)
+  return fold(
+    cause => Promise.reject(new Error(JSON.stringify(cause))),
+    (res: T) => Promise.resolve(res),
+  )(e)
 }
