30b720398c8e6346f8680530de154adba77befab
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-07-26 17:53:30 +0200

    Minor release 1.0.20

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -3,6 +3,7 @@ import * as t from 'io-ts';
 import { RepoRef } from './model/pullrequest';
 export declare const Config: t.ExactC<t.TypeC<{
     issueKeyRegex: t.StringC;
+    milestoneRegex: t.UnionC<[t.StringC, t.UndefinedC]>;
     fixVersionRegex: t.StringC;
     postMergeStatus: t.ArrayC<t.StringC>;
     githubDispatchBaseUrl: t.UnionC<[t.StringC, t.UndefinedC]>;
